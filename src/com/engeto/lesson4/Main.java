cf8211462f9e6c54d9532d63c865f988ec89d881
MiroslavHorejsi <mira.horejsi@gmail.com>
2022-02-21 21:46:13 +0100

    Add next text output, created and commited in IntelliJ.

--- src/com/engeto/lesson4/Main.java
+++ src/com/engeto/lesson4/Main.java
@@ -3,6 +3,10 @@ package com.engeto.lesson4;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Textový výpis do konzole");
+        System.out.println("Textový výpis do konzole");
+
+        System.out.println("Přidán další výpis, tentokát z IntelliJ");
+
+
     }
 }
